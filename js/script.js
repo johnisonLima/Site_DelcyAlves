2aba0c8020f102895eb12ed7fa710d686faf7ba6
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2023-07-15 12:02:26 -0300

    Ajustando imagens banner conforme a resolução

--- js/script.js
+++ js/script.js
@@ -2,23 +2,27 @@ window.onload = function(){
     // --------------------------------------------------------
     //  pegar cada imagem conforme a sua resolução
     // -------------------------------------------------------- 
-    var pegaMG = document.getElementsByClassName("carousel-item");
-    if(screen.width > 767){
-        for (var i=0; i<pegaMG.length; i++) {
-                pegaMG[i].lastElementChild.attributes.src.nodeValue = "carousel/header_background_v3_hd"+(i+1)+".jpg";            
-        }   
-    }
-    if(screen.width < 768 && screen.width > 480){
-        for (var i=0; i<pegaMG.length; i++) {         
-                pegaMG[i].lastElementChild.attributes.src.nodeValue = "carousel/header_background_tablet"+(i+1)+".jpg"
-            }   
-    }    
-    if(screen.width < 481){
-        for (var i=0; i<pegaMG.length; i++) {
-            console.log(
-                pegaMG[i].lastElementChild.attributes.src.nodeValue = "carousel/header_background_phone"+(i+1)+".jpg"
-            )
+    let imagem = document.getElementsByClassName("carousel-item");
 
+    let xSmall  = 575,
+        small   = 767,
+        medium  = 997,
+        large   = 1199,
+        xLarge  = 1399;
+
+    if(screen.width < medium){
+        for(let i=0; i<imagem.length; i++){            
+                imagem[i].lastElementChild.attributes.src.nodeValue = `carousel/xSmall/header_background_${i+1}.jpg`
         }   
     }
+    if(screen.width > medium && screen.width < large){
+        for(let i=0; i<imagem.length; i++){            
+                imagem[i].lastElementChild.attributes.src.nodeValue = `carousel/medium/header_background_${i+1}.jpg`
+        }   
+    }
+    if(screen.width > large){
+        for(let i=0; i<imagem.length; i++){            
+                imagem[i].lastElementChild.attributes.src.nodeValue = `carousel/large/header_background_${i+1}.jpg`
+        }   
+    }    
 }
