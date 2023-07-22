ad715bfc59c6c7b8c27c63e33a6b6b62a33953a8
Johnison Almeida Lima <123204896+johnisonLima@users.noreply.github.com>
2023-07-21 23:40:00 -0300

    Tentando faer um slidshow

--- js/script.js
+++ js/script.js
@@ -25,6 +25,51 @@ window.onload = function(){
                 imagem[i].lastElementChild.attributes.src.nodeValue = `carousel/large/header_background_${i+1}.jpg`
         }   
     }
+
+    // --------------------------------------------------------
+    // Slider Show
+    // -------------------------------------------------------- 
+    let carousel = document.querySelector('.carousel-track');
+    let slides = carousel.querySelectorAll('.carousel-slide');
+    
+    const carouselTrack = document.getElementById("carouselTrack");
+    
+
+    let position = 0;
+
+    function slide() {
+        position -= slides[nextIndex].clientWidth
+        slides[nextIndex].style.transform = `translateX(${position}px)`;
+
+        // Executa a função novamente após um intervalo de tempo
+        setTimeout(slide, 3000); // Tempo em milissegundos entre cada slide (3 segundos neste caso)
+    }
+
+    // Definir o slide ativo inicial
+    slides[0].classList.add('active');
+    
+    // Criar um temporizador que será acionado a cada 3 segundos
+    let timer = setInterval(function() {
+        // Obter o índice do slide ativo atual
+        let activeIndex = Array.from(slides).findIndex(function(slide) {
+            return slide.classList.contains('active');
+        });
+    
+        // Remover a classe ativa do slide atual
+        slides[activeIndex].classList.remove('active');
+        position = 0
+        slides[activeIndex].style.transform = `translateX(${position}px)`;
+    
+        // Calcular o índice do próximo slide
+        let nextIndex = (activeIndex + 1) % slides.length;
+    
+        // Definir o próximo slide como ativo
+        slides[nextIndex].classList.add('active');
+
+        position -= slides[nextIndex].clientWidth
+        slides[nextIndex].style.transform = `translateX(${position}px)`;
+    }, 5000);  
+      
     
     // --------------------------------------------------------
     //  Mudar a cor do header ao descer a página
